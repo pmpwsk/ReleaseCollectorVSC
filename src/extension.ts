5b7fadb29a1d2d12b095434f0d8b82899c6cbda5
Flo <flo@uwap.org>
2024-04-13 21:30:54 +0200

    Added code to detect the .csproj file.

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import * as fs from "./fs";
 
 export function activate(context: vscode.ExtensionContext)
 {
@@ -11,5 +12,26 @@ export function deactivate()
 
 async function publish()
 {
-	
+	var folders = vscode.workspace.workspaceFolders;
+	if (folders === undefined || folders.length !== 1)
+	{
+		vscode.window.showErrorMessage("No .csproj file found!");
+		return;
+	}
+	var projUri : vscode.Uri | undefined = folders[0].uri;
+	var projectFiles = (await fs.getFileNames(projUri)).filter(file => file.endsWith(".csproj"));
+	var projName : string;
+	switch (projectFiles.length)
+	{
+		case 0:
+			vscode.window.showErrorMessage("No .csproj file found!");
+			return;
+		case 1:
+			projName = projectFiles[0];
+			projName = projName.substring(0, projName.length - 7);
+			break;
+		default:
+			vscode.window.showErrorMessage("Multiple .csproj files found, please open one project specifically!");
+			return;
+	}
 }
